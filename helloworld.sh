115e869a0ce73e7b3bea95b57bb337e2549042cc
Tianna Farrow <raqueltianna@outlook.com>
2023-10-24 19:40:07 -0400

    added the helloworld.sh for first cybersecurity challenge.

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script Name: Ops Challenge: Class 02 
+# Author: Tianna Farrow
+# Date of latest revision: 10/24/2023
+# Purpose: First Ops Challenge for 201 course
+
+# Declaration of variables
+
+message="I am working perfectly!"
+
+# Declaration of functions 
+
+# Main
+
+echo "hello world"
+echo $"I am working perfectly fine!"
+
+# End 

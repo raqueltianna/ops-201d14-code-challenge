32be6404aa15e1de34fd0a10eda29a5e772ed18c
Tianna Farrow <raqueltianna@outlook.com>
2023-10-25 17:49:54 -0400

    This commit is to add a new page and run script.

---
+++ loginhistory.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Script Name:                       Loginhistory.sh
+#Author Name:                       Tianna Farrow 
+# Date of latest revision:          10/25/2023
+#Purpose:                           Create a function that prints Login History
+#Execution:                         bash loginhistory.sh or ./loginhistory.sh chmod -x loginhistory.sh
+
+# Decleration of the variables
+
+# Decleration of functions
+
+print_login_history() {
+    echo "This is the login history"
+    last
+
+}
+
+# Main
+
+print_login_history
+
+# End 

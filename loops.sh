483c464e7022cb068ade8b68da8c1157fead69d4
Tianna Farrow <raqueltianna@outlook.com>
2023-10-27 15:55:50 -0400

    created a new file to loop pid or cancel using ctrl+c

---
+++ loops.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# passing arguments to a function
+
+# Script Name:                          loops.sh
+# Author name:                          Tianna Farrow
+# Date of latest revision:              10/27/2023
+# Purpose:                              loop variables / and functions
+# Execution:                            bash loops.sh or ./loops.sh chmod -x loops.sh
+# Additional resources:                 https://www.quora.com/How-do-you-make-a-shell-script-run-continuously; https://www.linuxquestions.org/questions/programming-9/how-to-make-a-while-loop-iterate-over-script-until-user-input-stops-4175624869/
+
+# Decleration of the variables
+
+# Decleration of the functions
+
+while true; do
+    echo "Running processes:"
+    ps aux
+
+    read -p "Enter the PID of the process you want to kill or press Ctrl + C to exit:" pid
+
+    if ps -p "$pid" &> /dev/null; then
+        process_name=$(ps -p "$pid" -o comm=)
+        if [[ "process_name" != "kernel" && "$process_name" != "systemd" ]]; then
+            kill -9 "$pid"
+            echo "Process with PID $pid has been terminated." 
+        else
+            echo "Cannot kill essential OS process: $process_name"
+        fi
+    else
+        echo "No process found with PID $pid."
+    fi
+done
+
+
+
+# Main
+
+# End

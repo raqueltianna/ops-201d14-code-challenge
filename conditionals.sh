961d19bfb5088c92b8dc57b7f9cee6b0b884dd8c
Tianna Farrow <raqueltianna@outlook.com>
2023-10-30 17:02:27 -0400

    added my resources that I used to complete this.

--- conditionals.sh
+++ conditionals.sh
@@ -3,16 +3,15 @@
 # Script Name:                        Conditionals.sh
 # Author:                             Tianna Farrow 
 # Date of Latest Revision:            10/30/2023
-# Purpose:                            Demo conditionals using a grocery store list.
+# Purpose:                            Check for files/directories and create them if they do not exist.
 # Execution:                          bash conditionals.sh  or ./conditionals.sh chmod -x conditionals.sh
-# Additional resources:               
+# Additional resources:               https://github.com/codefellows/seattle-ops-201d14/tree/main/class-06/challenges; https://github.com/codefellows/seattle-ops-201d14/blob/main/class-06/demo/demo.sh
 
 # Decleration of the variables 
 
 items=("file.txt" "directory")
 
 # Decleration of functions
-
 
 for item in "${items[@]}"; do
     if [ -e "$item" ]; then
@@ -27,6 +26,6 @@ for item in "${items[@]}"; do
         fi
     fi
 done
-# Main
+
 
 # End
